1de369fc07abacd0ce27f6f5acfde7b03d656086
Ben0112358 <111197969+Ben0112358@users.noreply.github.com>
2025-07-30 20:52:03 +0200

    Alternative structure (#15)
    * new structure and improved network creation
    
    * linting

---
+++ globals.tf
@@ -0,0 +1,14 @@
+locals {
+  shared_template_vars = {
+    raw_data       = "${var.ml_homelab_root}/data/raw"
+    clean_data     = "${var.ml_homelab_root}/data/clean"
+    models         = "${var.ml_homelab_root}/models"
+    logs           = "${var.ml_homelab_root}/logs"
+    terraform_logs = "${var.ml_homelab_root}/logs/terraform"
+    data_logs      = "${var.ml_homelab_root}/logs/data"
+    training_logs  = "${var.ml_homelab_root}/logs/training"
+    serving_logs   = "${var.ml_homelab_root}/logs/serving"
+    ui_logs        = "${var.ml_homelab_root}/logs/ui"
+    pipeline_logs  = "${var.ml_homelab_root}/logs/pipeline"
+  }
+}
